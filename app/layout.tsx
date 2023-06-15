f09bf683949ddcf6ebde6b52fc212ff08a278fe2
AdylshaY <adylshay@gmail.com>
2023-06-15 14:36:46 +0300

    login and logout completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 import { Nunito } from 'next/font/google';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 const font = Nunito({
   subsets: ['latin'],
@@ -13,17 +15,20 @@ export const metadata = {
   description: 'Airbnb clone built with Next.js and Tailwind CSS',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang='en'>
       <body className={font.className}>
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
